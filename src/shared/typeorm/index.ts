271fb71ba7449a69349fec68f605d129cc6781f6
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-07-18 09:53:35 -0300

    criando entidade tabela pivo

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -8,6 +8,12 @@ import Product from '@modules/products/typeorm/entities/Product';
 import User from '@modules/users/typeorm/entities/User';
 import UserToken from '@modules/users/typeorm/entities/UserToken';
 import Customers from '@modules/customers/typeorm/entities/Customer';
+import { CreateOrders1689682023299 } from './migrations/1689682023299-CreateOrders';
+import { AddCustomerIdToOrders1689682388159 } from './migrations/1689682388159-AddCustomerIdToOrders';
+import { CreateOrdersProducts1689683124558 } from './migrations/1689683124558-CreateOrdersProducts';
+import { AddOrderIdToOrdersProducts1689683573418 } from './migrations/1689683573418-AddOrderIdToOrdersProducts';
+import { AddProductIdToOrdersProducts1689684004151 } from './migrations/1689684004151-AddProductIdToOrdersProducts';
+import Order from '@modules/orders/typeorm/entities/Order';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -16,11 +22,16 @@ export const dataSource = new DataSource({
   username: 'postgres',
   password: 'vanin2004',
   database: 'apivendas',
-  entities: [Product, User, UserToken, Customers],
+  entities: [Product, User, UserToken, Customers, Order],
   migrations: [
     CreateProducts1607437608841,
     CreateUsers1689532761946,
     CreateUserTokens1689597162328,
     CreateCustomers1689632565532,
+    CreateOrders1689682023299,
+    AddCustomerIdToOrders1689682388159,
+    CreateOrdersProducts1689683124558,
+    AddOrderIdToOrdersProducts1689683573418,
+    AddProductIdToOrdersProducts1689684004151,
   ],
 });
