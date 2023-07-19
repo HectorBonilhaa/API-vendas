6938ace67c45855d56d15a5aa78754caae959fed
HectorBonilhaa <hectoroliveira1@gmail.com>
2023-07-19 09:19:40 -0300

    finalizado crud orders

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import AppError from '@shared/errors/AppError';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order | null> {
+    const ordersRepository = new OrdersRepository();
+
+    const order = await ordersRepository.findById(id);
+
+    if (!order) {
+      throw new AppError('Order not found');
+    }
+
+    return order;
+  }
+}
+export default ShowOrderService;
